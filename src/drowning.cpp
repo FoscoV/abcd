ce33cbf0d7c0cac4ebed30afff406cb7a402cccb
Fosco <fosco.vesely@gmail.com>
2017-10-25 17:45:59 +0200

    auxiliary function

---
+++ src/drowning.cpp
@@ -0,0 +1,25 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List waterMove(NumericVector neighs;NumericVector neigValues; double waterLevel){
+
+
+}
+
+//prepare the grid of values
+
+//find the minimum
+
+//include minimum in the grid
+
+
+
+
+//look for neighs
+
+//neighs expected value
+
+//minimum of neighs
+
+//waterfill
